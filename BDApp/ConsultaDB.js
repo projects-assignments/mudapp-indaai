686389a86b192a416cc11bc0eb97d3e2933fe2ca
aetior <135319488+aetior@users.noreply.github.com>
2024-01-16 10:16:03 +0100

    nueva funcion ACTUALIZAR USUARIO

--- BDApp/ConsultaDB.js
+++ BDApp/ConsultaDB.js
@@ -38,17 +38,30 @@ async function busqueda(client, busqueda) {
     const result = await client.db("miapp").collection("Usuario").insertOne(add);
     console.log(result);
   }
-                                     // CREAR MUCHOS USUARIOS
+                                     // CREAR VARIOS USUARIOS
   async function addClientes(client, add) {
-    const result = await client.db("miapp").collection("Usuario").insertMany(add);
+    const result = await client.db("miapp").collection("Servicio").insertMany(add);
     console.log(result);
   }
+                             //////// ACTUALIZAR USUARIO
+  async function actualizarUsuario(client, filtro, nuevosValores) {
+      try {
+            const result = await client.db("miapp").collection("Usuario").updateOne(filtro, { $set: nuevosValores });
+            console.log(`Usuario actualizado: ${result.modifiedCount} documento(s) modificado(s)`);                                       
+               } catch (error) {               
+                 console.error("Error al actualizar el usuario", error);     
+                       }       
+                       } 
 
-
-
-
-  // busqueda(client, {
-  //   _id: "2"
+          
+                            //////// EJEMPLOS
+    actualizarUsuario(client, { _id: "2" }, { 
+      Nombre: "DANIELA", DNI: "3423a" });                  
+  
+  
+  //   busqueda(client, {
+  //   _id: "2",
+  //   nombre: "Ricardo1"
   // });
   
   // addCliente(client,{
@@ -57,14 +70,27 @@ async function busqueda(client, busqueda) {
   //   DNI:"4323432A"
   // })
 
-  addClientes(client,[{
-    _id: "134",
-    nombre: "Ricardo1",
-    DNI:"4323432A"
-  },{
-    _id: "145",
-    nombre: "Ricardo2",
-    DNI:"4323432A"
-  }])
+//   addClientes(client,[{
+//     "_id":"454",
+//     "Fecha":"08/11/22",
+//     "Estado":"Enviado",
+//     "IdTransportista":"7",
+//     "idUsuario":"1",
+//     "DireccionSalida/Destino":"C/ElmStreet to Rmb/Catalunya"
+// },{
+//     "_id":"5334",
+//     "Fecha":"06/11/22",
+//     "Estado":"En Camino",
+//     "IdTransportista":"8",
+//     "idUsuario":"2",
+//     "DireccionSalida/Destino":"Rmb/Castell to Rmb/Catalunya"
+// },{
+//     "_id":"534",
+//     "Fecha":"04/11/22",
+//     "Estado":"Enviado",
+//     "IdTransportista":"9",
+//     "idUsuario":"3",
+//     "DireccionSalida/Destino":"C/ElmStreet to Rmb/Castell"
+// }])
 
   
